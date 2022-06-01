a55ad5c76405f8c5757126a389268e4631dcc1e8
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-06-01 17:50:08 +0200

    Lada class

--- src/modell/Lada.java
+++ src/modell/Lada.java
@@ -3,5 +3,15 @@ package modell;
 
 
 public class Lada {
+    public static final String BRONZ = "bronz";
+    public static final String EZUST = "ezüst";
+    public static final String ARANY = "arany";
     
+    public boolean talaltE(String tipp){
+        if (tipp.equals(Lada.EZUST)) {
+            return true;
+        }
+        return false;
+    }
+
 }
