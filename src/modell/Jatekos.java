e8486dd39d35c4ce8f432b291c5d4c374869c5a3
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-06-04 21:45:58 +0200

    Jatekos class nem szukseges

--- src/modell/Jatekos.java
+++ src/modell/Jatekos.java
@@ -1,20 +1,20 @@
-
-package modell;
-
-public class Jatekos {
-    private String tipp;
-
-    public Jatekos() {
-        this.tipp = "";
-    }
-
-    public String getTipp() {
-        return tipp;
-    }
-
-    public void setTipp(String tipp) {
-        this.tipp = tipp;
-    }
-    
-    
-}
+//
+//package modell;
+//
+//public class Jatekos {
+//    private String tipp;
+//
+//    public Jatekos() {
+//        this.tipp = "";
+//    }
+//
+//    public String getTipp() {
+//        return tipp;
+//    }
+//
+//    public void setTipp(String tipp) {
+//        this.tipp = tipp;
+//    }
+//    
+//    
+//}
